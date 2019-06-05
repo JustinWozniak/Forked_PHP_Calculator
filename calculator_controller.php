2f248f286db7b003b83b9cf1fd740acdaa8852c9
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-06-05 16:15:31 -0400

    More cleanup. renamed number1 to message_text in the form.

--- calculator_controller.php
+++ calculator_controller.php
@@ -6,22 +6,21 @@ CalcDB_Connect();
 
 
 // if the user clicks the "Clear" input
-if( isset($_POST['clear_history']) ) {
+if (isset($_POST['clear_history'])) {
     HistoryOfCalculations_ClearAll();
 }
 
 
 // the following it TRUE if form has been submitted, else FALSE
-$form_has_been_submitted = isset($_POST['number1']);
+$form_has_been_submitted = isset($_POST['message_text']);
 
 // defaults, in case the form has not been submitted
-$number1 = "";
 $message_text = "";
 
 // only process form variables if the form was submitted... avoids errors
-if( $form_has_been_submitted ) {
-    $message_text = $_POST['number1'];
-    
+if ($form_has_been_submitted) {
+    $message_text = $_POST['message_text'];
+
     HistoryOfCalculations_AddLine($message_text);
 }
 
@@ -30,6 +29,3 @@ $history_of_calculations_array = HistoryOfCalculations_GetAll();
 
 
 CalcDB_Close();
-
-
-?>
