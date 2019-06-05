0b5b94c3aa65bbd2c31fd0d6bece6bc4aadcc69f
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-06-05 15:50:42 -0400

    cleaned up and input box is now blank after you submit a message

--- calculator.php
+++ calculator.php
@@ -4,39 +4,39 @@ include("calculator_controller.php");
 
 ?>
 
-
 <!DOCTYPE html>
 <html>
+
 <head>
-	<style>
-    h2 {
-        font-family: cursive;
-    }
-	</style>
+    <style>
+        h2 {
+            font-family: cursive;
+        }
+    </style>
 </head>
+
 <body>
 
-        
-        
     </form>
-    
+
     <h2>Messages</h2>
     <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-      
+
     </form>
     <ul>
         <?php
-        foreach($history_of_calculations_array as $line) {
+        foreach ($history_of_calculations_array as $line) {
             echo "<li>$line</li>";
         }
         ?>
     </ul>
     <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-        <input type="text" name="number1" value="<?= $number1 ?>">
-        
+        <input type="text" name="number1" value="">
+
         <input type="submit" name="add" value="+">
         <br />
         <br />
         <input type="submit" name="clear_history" value="clear">
 </body>
+
 </html>
